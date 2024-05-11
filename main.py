aeb5d3ab905cd6e476f187558686cd3106a7c47b
Ivan-Ps <alcarras.ivan.pelegri@gmail.com>
2024-05-11 18:23:21 +0200

    add config system

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from sqlalchemy import MetaData
 import routes
+from Configurations import Configuration
+from fastapi_sqlalchemy import DBSessionMiddleware 
 
 app = FastAPI()
+
+app.add_middleware(DBSessionMiddleware, db_url=Configuration.get_db_url())
 app.include_router(routes.router)
 
 
